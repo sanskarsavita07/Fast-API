6dd1d0640037076b1b9fbee4c3b6847b81406bf2
sanskarsavita07 <sharmasanskar196@gmail.com>
2025-12-12 17:19:31 +0530

    "summary"
    sample code

---
+++ fastapi.py
@@ -0,0 +1,36 @@
+# #API 
+
+# #FASTAPI - fast to make api types of data we want in an api or information , data validation, json
+
+# #data validation - starting and end points 
+# #auto documentation 
+# #auto completion and code suggestions
+
+# from fastapi import FastAPI
+
+# app = FastAPI() #create api object with variable app
+
+# #END POINT - An API endpoint is the point of entry in a communication channel when two systems are interacting. 
+# # It refers to touchpoints of the communication between an API and a server.
+
+# #example
+# #      /hello
+# #     /get-item
+
+# # localhost/hello - base url is localhost and endpoint is /hello
+
+# #exapme - facebook.com/home
+
+# #GET - it will return information hhtps method
+# #POST - it will sending information to the post endpoint or method post will create new user signup
+# #PUT - it is used to update the values existing in the database 
+# #DELETE - it is used to delete information from database
+
+# @app.get("/") #endpoint home get method used 
+# def home(): #function 
+#     return {"Data": "Test"} #data or infromation store on route
+
+
+
+
+
